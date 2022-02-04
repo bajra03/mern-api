93ef96cdfa49ed394f59aaeef68f34c450d23699
bajra <bajra.idabagus@karmagroup.com>
2022-02-04 15:30:29 +0800

    add blogpost model

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const port = 4000
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 4000;
+const mongoose = require('mongoose');
 
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-app.use(express.json()) // for parsing application/json
 
-const blogRoutes = require('./src/routes/blogs')
-const authRoutes = require('./src/routes/auth')
 
-app.use(cors())
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.json()); // for parsing application/json
 
-app.use('/v1/blog', blogRoutes)
-app.use('/v1/auth', authRoutes)
+const blogRoutes = require('./src/routes/blogs');
+const authRoutes = require('./src/routes/auth');
+
+app.use(cors());
+
+app.use('/v1/blog', blogRoutes);
+app.use('/v1/auth', authRoutes);
 
 // Create handling global error
 app.use((error, req, res, next) => {
@@ -24,8 +27,13 @@ app.use((error, req, res, next) => {
     message: message,
     data: data
   })
-})
-
-app.listen(port, () => {
-  console.log("Server running on port: ", port)
 });
+
+// connect the database
+mongoose.connect('mongodb+srv://bajra:F4fSkhTzfisdBsvn@cluster0.neezo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  .then(() => {
+    app.listen(port, () => {
+      console.log("Server running on port: ", port);
+    });
+  })
+  .catch(err => console.log(err));
