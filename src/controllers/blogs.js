7ec91c2917750c5408a3c8fd810beb562a3cacb6
bajra <bajra.idabagus@karmagroup.com>
2022-03-22 15:44:21 +0800

    add remove post function

--- src/controllers/blogs.js
+++ src/controllers/blogs.js
@@ -1,6 +1,9 @@
 const { validationResult } = require('express-validator');
 const BlogPost = require('../models/blogs');
+const path = require('path');
+const fs = require('fs'); // File System
 
+// create Post
 exports.createBlogPost = (req, res, next) => {
   const errors = validationResult(req);
 
@@ -46,6 +49,7 @@ exports.createBlogPost = (req, res, next) => {
   
 }
 
+// Get all posts
 exports.getBlogsPost = (req, res, next) => {
   BlogPost.find()
     .then(
@@ -59,4 +63,106 @@ exports.getBlogsPost = (req, res, next) => {
     .catch(err => {
       next(err)
     });
+}
+
+// Get post by ID
+exports.getBlogPostById = (req, res, next) => {
+  const postId = req.params.postId;
+  BlogPost.findById(postId)
+    .then(result => {
+      if (!result) {
+        const error = new Error('Blog Post not found!');
+        error.errorStatus = 400;
+        throw error;
+      }
+
+      res.status(200).json({
+        message: 'Get Post by id success',
+        data: result
+      })
+    })
+    .catch(err => {
+      next(err)
+    })
+}
+
+// Update blog post by id
+exports.updateBlogPost = (req, res, next) => {
+  const errors = validationResult(req);
+
+  // check if data error/not valid
+  if (!errors.isEmpty()) {
+    const err = new Error('Input values not valid.');
+    err.errorStatus = 400;
+    err.data = errors.array();
+    throw err;
+  }
+
+  // check image upload/not
+  if (!req.file) {
+    const err = new Error('Image must be uploaded!');
+    err.errorStatus = 422;
+    throw err;
+  }
+
+  const title = req.body.title;
+  const image = req.file.path; // only request the url path from request
+  const body = req.body.body;
+  const postId = req.params.postId;
+
+  BlogPost.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Blog post not found!');
+        error.errorStatus = 404;
+        throw error;
+      }
+
+      post.title = title;
+      post.body = body;
+      post.image = image;
+
+      return post.save();
+    })
+    .then(result => {
+      res.status(200).json({
+        message: 'Bolg Post updated!',
+        data: result
+      })
+    })
+    .catch(err => {
+      next(err);
+    })
+}
+
+// Delete blog post
+exports.deleteBlogPost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  BlogPost.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Blog post not found!');
+        error.errorStatus = 404;
+        throw error;
+      }
+
+      removeImage(post.image);
+      return BlogPost.findByIdAndRemove(postId);
+    })
+    .then(result => {
+      res.status(200).json({
+        message: 'Blog post has been deleted',
+        data: result
+      })
+    })
+    .catch(err => {
+      next(err);
+    });
+}
+
+// function remove image
+const removeImage = (filePath) => {
+  filePath = path.join(__dirname, '../..', filePath);
+  fs.unlink(filePath, err => console.log(err));
 }
--- src/routes/blogs.js
+++ src/routes/blogs.js
@@ -7,6 +7,9 @@ const blogsController = require('../controllers/blogs');
 // Get all blog
 router.get('/post', blogsController.getBlogsPost);
 
+// Get Post by ID
+router.get('/post/:postId', blogsController.getBlogPostById);
+
 // Create new blog
 router.post(
   '/post',
@@ -16,5 +19,17 @@ router.post(
   ],
   blogsController.createBlogPost
 );
+
+// Update blog post
+router.put('/post/:postId',
+  [
+    body('title').isLength({ min: 5 }).withMessage('Title minimal 5 character'),
+    body('body').isLength({ min: 5 }).withMessage('Body content minimal 5 character')
+  ],
+  blogsController.updateBlogPost
+);
+
+// Delete blog post
+router.delete('/post/:postId', blogsController.deleteBlogPost);
 
 module.exports = router;
